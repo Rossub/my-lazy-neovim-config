05e3c1563eeb95e7e0c405245b6f6616609c97e8
Rossub <rosh2060an@gmail.com>
2023-05-06 11:09:37 +0545

    My first commit

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "navarasu/onedark.nvim",
+
+    opts = {
+      --style = "cool",
+      style = "deep",
+      --style = "dark",
+      --style = "darker",
+      --style = "warm",
+      --style = "warmer",
+    },
+  },
+  { "sainnhe/edge" },
+  { "neanias/everforest-nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "cocopon/iceberg.vim" },
+  { "sainnhe/gruvbox-material" },
+  { "sonph/onehalf" },
+  { "sainnhe/sonokai" },
+  { "folke/tokyonight.nvim" },
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "Mofiqul/dracula.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+      --colorscheme = "catppuccin-mocha",
+    },
+  },
+}
